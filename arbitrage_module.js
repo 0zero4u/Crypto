069c58377932b624b3b46d3c540ef8748eb1fe01
Heck <97088365+0zero4u@users.noreply.github.com>
2025-05-30 18:52:19 +0530

    Update arbitrage_module.js

--- arbitrage_module.js
+++ arbitrage_module.js
@@ -57,7 +57,7 @@ const ARB_PING_INTERVAL_MS = 3 * 60 * 1000;
 const DESIRED_PROFIT_THRESHOLD_USD = 5.0;
 const TOTAL_FEES_PER_UNIT_USD = 0.2;
 const ARBITRAGE_CHECK_INTERVAL_MS = 20;
-const NATURAL_BASIS_SPOT_OVER_FUTURES = 48.0;
+const NATURAL_BASIS_SPOT_OVER_FUTURES = 48.5;
 
 // --- Arbitrage State Variables ---
 let internalWsClient = null;
