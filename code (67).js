f0d7eeed6b7f4f8bea2ef72ab58ec1e7741ca2d1
Heck <97088365+0zero4u@users.noreply.github.com>
2025-06-13 19:59:39 +0530

    Delete code (67).js

--- code (67).js
+++
@@ -1,4 +0,0 @@
-        // NEW, STATE-AWARE LOGIC
-        const dynamicThreshold = SCORE_THRESHOLDS_BY_BIAS[lastSentPrediction.bias] || 1.5;
-        const scoreChangedSignificantly = Math.abs(score - lastSentPrediction.score) > dynamicThreshold;
-        const biasChanged = currentBias !== lastSentPrediction.bias;
