dff7a3444cd3339c824ccd5656b6a887334b1129
Heck <97088365+0zero4u@users.noreply.github.com>
2025-08-05 15:40:12 +0530

    Update binance_listener.js

--- binance_listener.js
+++ binance_listener.js
@@ -38,7 +38,7 @@ function cleanupAndExit(exitCode = 1) {
 // --- Configuration ---
 const SYMBOL = 'btcusdt'; // Binance uses lowercase for streams
 const RECONNECT_INTERVAL_MS = 5000;
-const MINIMUM_TICK_SIZE = 0.1;
+const MINIMUM_TICK_SIZE = 0.2;
 
 // Using the correct internal DNS for service-to-service communication in GCP
 const internalReceiverUrl = 'ws://instance-20250627-040948.asia-south2-a.c.ace-server-460719-b7.internal:8082/internal';
